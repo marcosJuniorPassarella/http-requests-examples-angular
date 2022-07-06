be3f33609ee3053fe4f2b5144273f4c0fc6fb7ac
MJWORKS <mjworks2020@outlook.com>
2022-07-06 20:15:53 -0300

    Created service and respective methods

---
+++ src/app/services/posts.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map, Observable, Subscription } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Post } from '../models/post.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PostsService {
+  constructor(private http: HttpClient) {}
+
+  createPost(post: Post): Subscription {
+    return this.http
+      .post<{ name: string }>(`${environment.firebaseUrlApi}/posts.json`, post)
+      .subscribe({
+        next: (res) => console.log(res),
+        error: (err) => console.log(err),
+      });
+  }
+
+  fetchPosts(): Observable<any> {
+    return this.http
+      .get<{ [key: string]: Post }>(`${environment.firebaseUrlApi}/posts.json`)
+      .pipe(
+        map((responseData) => {
+          const postsArrray: Post[] = [];
+          for (const key in responseData) {
+            if (responseData.hasOwnProperty(key)) {
+              postsArrray.push({ ...responseData[key], id: key });
+            }
+          }
+          return postsArrray;
+        })
+      );
+  }
+}
